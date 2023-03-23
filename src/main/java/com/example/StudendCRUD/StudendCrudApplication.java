55fa0a58bfe3845f2df5f5b819ce703c3f1312a5
DIVYA SINGH <divyasinghdibs@gmail.com>
2023-03-23 16:14:42 +0530

    Student course management system with simple CRUD operations in sping boot java

---
+++ src/main/java/com/example/StudendCRUD/StudendCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudendCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudendCrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/StudentCRUD/domain/Student.java
@@ -0,0 +1,57 @@
+package com.example.StudentCRUD.domain;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Student {
+	
+@Id
+@GeneratedValue(strategy= GenerationType.IDENTITY)
+
+private Long id;
+private String studentName;
+private String course;
+private int fee;
+public Student() {
+	super();
+	// TODO Auto-generated constructor stub
+	
+	
+}
+public Student(Long id, String studentName, String course, int fee) {
+	super();
+	this.id = id;
+	this.studentName = studentName;
+	this.course = course;
+	this.fee = fee;
+}
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getStudentName() {
+	return studentName;
+}
+public void setStudentName(String studentName) {
+	this.studentName = studentName;
+}
+public String getCourse() {
+	return course;
+}
+public void setCourse(String course) {
+	this.course = course;
+}
+public int getFee() {
+	return fee;
+}
+public void setFee(int fee) {
+	this.fee = fee;
+}
+
+}
